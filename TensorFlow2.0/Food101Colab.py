89e9b6974316740ffb443255827cfc12c9b074b8
Avinash <avinash.kappa@gmail.com>
2019-02-17 23:28:57 +0530

    PracticeGround - Added Food101Colab.py

---
+++ TensorFlow2.0/Food101Colab.py
@@ -0,0 +1,65 @@
+import keras.backend as K
+from keras import regularizers
+from keras.applications.inception_v3 import InceptionV3
+from keras.models import Sequential, Model, Input
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, AveragePooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras.optimizers import SGD
+from keras.regularizers import l2
+import numpy as np
+
+K.clear_session()
+
+n_classes = 3
+img_width, img_height = 299, 299
+train_data_dir = 'food-101/train_mini'
+validation_data_dir = 'food-101/test_mini'
+nb_train_samples = 2250 #75750
+nb_validation_samples = 750 #25250
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+
+inception = InceptionV3(weights='imagenet', include_top=False)
+x = inception.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(128,activation='relu')(x)
+x = Dropout(0.2)(x)
+
+predictions = Dense(3,kernel_regularizer=regularizers.l2(0.005), activation='softmax')(x)
+
+model = Model(inputs=inception.input, outputs=predictions)
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+checkpointer = ModelCheckpoint(filepath='best_model_11class.hdf5', verbose=1, save_best_only=True)
+csv_logger = CSVLogger('history.log')
+
+history_11class = model.fit_generator(train_generator,
+                    steps_per_epoch = nb_train_samples // batch_size,
+                    validation_data=validation_generator,
+                    validation_steps=nb_validation_samples // batch_size,
+                    epochs=30,
+                    verbose=1,
+                    callbacks=[csv_logger, checkpointer])
+
+model.save('model_trained_11class.hdf5')
